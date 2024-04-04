d583f15db6b6c4c060d0ae1415f71eb5206c5056
BuuzaMan <batueveuror@gmail.com>
2024-04-05 03:06:36 +0800

    almost done

--- src/footer/details/messengers/index.js
+++ src/footer/details/messengers/index.js
@@ -1,6 +1,5 @@
 "use client"
 import Telegram from '@/svg/telegram'
-import VK from '@/svg/vk';
 import Whatsapp from '@/svg/whatsapp'
 import React from 'react'
 
@@ -11,15 +10,12 @@ const Messengers = () => {
     const handleTelegram = () => {
         window.open('https://t.me/pitergsmchat', '_blank')
     };
-    const handleVK = () => {
-        window.open('https://vk.com/pitergsm', '_blank')
-    }
+   
     
   return (
     <div className='flex flex-row desktopxl:gap-x-4 desktopmd:gap-x-4 desktopsm:gap-x-4 tablet:gap-x-4 tabletsm:gap-x-4 mobilexl:gap-x-4 mobilemd:gap-x-4 mobilesm:gap-x-3 desktopxl:mt-[20px] desktopmd:mt-[20px] desktopsm:mt-[20px] tablet:mt-[20px] tabletsm:mt-[20px] mobilexl:mt-[20px] mobilemd:mt-[20px] mobilesm:mt-[24px] desktopxl:mb-[170px] desktopmd:mb-[119px] desktopsm:mb-[119px] tablet:mb-[119px] tabletsm:mb-[119px] mobilexl:mb-[119px] mobilemd:mb-[119px] mobilesm:mb-[56px]'>
         <button onClick={handleWhatsapp}><Whatsapp /></button>
         <button onClick={handleTelegram}><Telegram /></button>
-        <button onClick={handleVK}><VK /></button>
     </div>
   )
 };
--- src/footer/index.js
+++ src/footer/index.js
@@ -12,7 +12,7 @@ const Footer = () => {
                 <p className='font-sf_text font-semibold desktopxl:text-[32px] desktopmd:text-[32px] desktopsm:text-[32px] tablet:text-[32px] tabletsm:text-[32px] mobilexl:text-[32px] mobilemd:text-[30px] mobilesm:text-[26px] uppercase bg-gradient-to-r from-[#2E84C3] via-[#52A27E] to-[#73BE3B] w-[310px] bg-clip-text text-transparent desktopxl:mb-[20px] desktopmd:mb-[20px] desktopsm:mb-[20px] tablet:mb-[20px] tabletsm:mb-[20px] mobilexl:mb-[20px] mobilemd:mb-[20px] mobilesm:mb-[12px]'>всегда на связи</p>
                 <a href="tel:+78122440079" className='font-sf font-extrabold desktopxl:text-[40px] desktopmd:text-[40px] desktopsm:text-[40px] tablet:text-[40px] tabletsm:text-[40px] mobilexl:text-[40px] mobilemd:text-[40px] mobilesm:text-[32px] leading-[1rem] -tracking-[.03em]'>+7 (812) 244 00 79</a>
                 <Messengers />
-                <MainLogo />
+                <div className='w-[200px]'><img src='images/logo.png' alt='mainlogo' /></div>
                 <div className='font-sf_text font-normal desktopxl:mt-[45px] desktopmd:mt-[90px] desktopsm:mt-[90px] tablet:mt-[90px] tabletsm:mt-[130px] mobilexl:mt-[100px] mobilemd:mt-[40px] mobilesm:mt-[25px] text-gray-400 desktopxl:text-[12px] desktopmd:text-[12px] desktopsm:text-[12px] tablet:text-[12px] tabletsm:text-[12px] mobilexl:text-[12px] mobilemd:text-[12px] mobilesm:text-[12px]'>© pitergsm.ru, 2024. Все права защищены</div>
             </div>
         </div>
